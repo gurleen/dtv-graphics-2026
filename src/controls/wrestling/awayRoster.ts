2379b832f6af366f0128b5c4effea44149cf8293
Gurleen Singh <gs585@drexel.edu>
2026-01-02 10:15:08 -0500

    Updated away wrestling roster to Purdue

--- src/controls/wrestling/awayRoster.ts
+++ src/controls/wrestling/awayRoster.ts
@@ -1,244 +1,253 @@
 import type { Wrestler } from "./types";
 
-export const AwayRoster: Wrestler[] = [
-  {
-    "firstName": "Josh",
-    "lastName": "Barr",
-    "primaryWeightClass": 197,
-    "secondaryWeightClass": undefined,
-    "year": 2,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Sam",
-    "lastName": "Beckett",
-    "primaryWeightClass": 165,
-    "secondaryWeightClass": undefined,
-    "year": 5,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Marcus",
-    "lastName": "Blaze",
-    "primaryWeightClass": 133,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Dawson",
-    "lastName": "Bundy",
-    "primaryWeightClass": 285,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": true
-  },
-  {
-    "firstName": "Lucas",
-    "lastName": "Cochran",
-    "primaryWeightClass": 285,
-    "secondaryWeightClass": undefined,
-    "year": 4,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Asher",
-    "lastName": "Cunningham",
-    "primaryWeightClass": 174,
-    "secondaryWeightClass": 184,
-    "year": 1,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Hayden",
-    "lastName": "Cunningham",
-    "primaryWeightClass": 141,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": true
-  },
-  {
-    "firstName": "Braeden",
-    "lastName": "Davis",
-    "primaryWeightClass": 141,
-    "secondaryWeightClass": undefined,
-    "year": 3,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Sean",
-    "lastName": "Degl",
-    "primaryWeightClass": 165,
-    "secondaryWeightClass": 174,
-    "year": 1,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Nate",
-    "lastName": "Desmond",
-    "primaryWeightClass": 125,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "PJ",
-    "lastName": "Duke",
-    "primaryWeightClass": 157,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Mason",
-    "lastName": "Ellis",
-    "primaryWeightClass": 184,
-    "secondaryWeightClass": 197,
-    "year": 1,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Kyison",
-    "lastName": "Garcia",
-    "primaryWeightClass": 133,
-    "secondaryWeightClass": undefined,
-    "year": 2,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Levi",
-    "lastName": "Haines",
-    "primaryWeightClass": 174,
-    "secondaryWeightClass": undefined,
-    "year": 4,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "William",
-    "lastName": "Henckel",
-    "primaryWeightClass": 174,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Tyler",
-    "lastName": "Kasak",
-    "primaryWeightClass": 157,
-    "secondaryWeightClass": undefined,
-    "year": 3,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Luke",
-    "lastName": "Lilledahl",
-    "primaryWeightClass": 125,
-    "secondaryWeightClass": undefined,
-    "year": 2,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Mitchell",
-    "lastName": "Mesenbrink",
-    "primaryWeightClass": 165,
-    "secondaryWeightClass": undefined,
-    "year": 3,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Cole",
-    "lastName": "Mirasola",
-    "primaryWeightClass": 285,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": true
-  },
-  {
-    "firstName": "Connor",
-    "lastName": "Mirasola",
-    "primaryWeightClass": 197,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": true
-  },
-  {
-    "firstName": "Aaron",
-    "lastName": "Nagao",
-    "primaryWeightClass": 141,
-    "secondaryWeightClass": undefined,
-    "year": 3,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Cael",
-    "lastName": "Nasdeo",
-    "primaryWeightClass": 141,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": true
-  },
-  {
-    "firstName": "Masanosuke",
-    "lastName": "Ono",
-    "primaryWeightClass": 133,
-    "secondaryWeightClass": undefined,
-    "year": 3,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Dalton",
-    "lastName": "Perry",
-    "primaryWeightClass": 149,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Connor",
-    "lastName": "Pierce",
-    "primaryWeightClass": 149,
-    "secondaryWeightClass": undefined,
-    "year": 3,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Joe",
-    "lastName": "Sealey",
-    "primaryWeightClass": 157,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": true
-  },
-  {
-    "firstName": "Shayne",
-    "lastName": "Van Ness",
-    "primaryWeightClass": 149,
-    "secondaryWeightClass": undefined,
-    "year": 3,
-    "isRedshirt": false
-  },
-  {
-    "firstName": "Ty",
-    "lastName": "Watson",
-    "primaryWeightClass": 165,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": true
-  },
-  {
-    "firstName": "Brock",
-    "lastName": "Weiss",
-    "primaryWeightClass": 149,
-    "secondaryWeightClass": undefined,
-    "year": 1,
-    "isRedshirt": true
-  },
-  {
-    "firstName": "Rocco",
-    "lastName": "Welsh",
-    "primaryWeightClass": 184,
-    "secondaryWeightClass": undefined,
-    "year": 2,
-    "isRedshirt": false
-  }
-]
+export const AwayRoster: Wrestler[] =
+  [
+    {
+      "firstName": "Brody",
+      "lastName": "Baumann",
+      "primaryWeightClass": 174,
+      "secondaryWeightClass": undefined,
+      "year": 3,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Joey",
+      "lastName": "Blaze",
+      "primaryWeightClass": 165,
+      "secondaryWeightClass": undefined,
+      "year": 3,
+      "isRedshirt": false
+    },
+    {
+      "firstName": "Blake",
+      "lastName": "Boarman",
+      "primaryWeightClass": 133,
+      "secondaryWeightClass": undefined,
+      "year": 4,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Gavin",
+      "lastName": "Brown",
+      "primaryWeightClass": 149,
+      "secondaryWeightClass": undefined,
+      "year": 3,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Stoney",
+      "lastName": "Buell",
+      "primaryWeightClass": 157,
+      "secondaryWeightClass": undefined,
+      "year": 4,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Dominic",
+      "lastName": "Burgett",
+      "primaryWeightClass": 285,
+      "secondaryWeightClass": undefined,
+      "year": 2,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Greyson",
+      "lastName": "Clark",
+      "primaryWeightClass": 141,
+      "secondaryWeightClass": undefined,
+      "year": 3,
+      "isRedshirt": false
+    },
+    {
+      "firstName": "Aidan",
+      "lastName": "Costello",
+      "primaryWeightClass": 174,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": false
+    },
+    {
+      "firstName": "Hayden",
+      "lastName": "Filipovich",
+      "primaryWeightClass": 285,
+      "secondaryWeightClass": undefined,
+      "year": 4,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Ty",
+      "lastName": "Henderson",
+      "primaryWeightClass": 125,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": false
+    },
+    {
+      "firstName": "Quinn",
+      "lastName": "Herbert",
+      "primaryWeightClass": 184,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Ashton",
+      "lastName": "Jackson",
+      "primaryWeightClass": 125,
+      "secondaryWeightClass": undefined,
+      "year": 2,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Wyatt",
+      "lastName": "Krejsa",
+      "primaryWeightClass": 149,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Kade",
+      "lastName": "Law",
+      "primaryWeightClass": 157,
+      "secondaryWeightClass": undefined,
+      "year": 3,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Jacob",
+      "lastName": "Macatangay",
+      "primaryWeightClass": 125,
+      "secondaryWeightClass": undefined,
+      "year": 3,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Keagan",
+      "lastName": "Martin",
+      "primaryWeightClass": 285,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Dustin",
+      "lastName": "Norris",
+      "primaryWeightClass": 133,
+      "secondaryWeightClass": undefined,
+      "year": 4,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Vincent",
+      "lastName": "Paino",
+      "primaryWeightClass": 133,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": false
+    },
+    {
+      "firstName": "Adrian",
+      "lastName": "Pellot",
+      "primaryWeightClass": 165,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": false
+    },
+    {
+      "firstName": "Ethan",
+      "lastName": "Popp",
+      "primaryWeightClass": 165,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "RJ",
+      "lastName": "Powers",
+      "primaryWeightClass": 184,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Isaiah",
+      "lastName": "Quintero",
+      "primaryWeightClass": 125,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Jaden",
+      "lastName": "Reynolds",
+      "primaryWeightClass": 141,
+      "secondaryWeightClass": undefined,
+      "year": 4,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "James",
+      "lastName": "Rowley",
+      "primaryWeightClass": 184,
+      "secondaryWeightClass": undefined,
+      "year": 3,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Isaac",
+      "lastName": "Ruble",
+      "primaryWeightClass": 157,
+      "secondaryWeightClass": undefined,
+      "year": 3,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Tyson",
+      "lastName": "Russell",
+      "primaryWeightClass": 285,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": false
+    },
+    {
+      "firstName": "Brody",
+      "lastName": "Saccoccia",
+      "primaryWeightClass": 157,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": false
+    },
+    {
+      "firstName": "Isaiah",
+      "lastName": "Schaefer",
+      "primaryWeightClass": 133,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": false
+    },
+    {
+      "firstName": "Cole",
+      "lastName": "Solomey",
+      "primaryWeightClass": 149,
+      "secondaryWeightClass": undefined,
+      "year": 2,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Ben",
+      "lastName": "Vanadia",
+      "primaryWeightClass": 197,
+      "secondaryWeightClass": undefined,
+      "year": 4,
+      "isRedshirt": true
+    },
+    {
+      "firstName": "Noah",
+      "lastName": "Weaver",
+      "primaryWeightClass": 197,
+      "secondaryWeightClass": undefined,
+      "year": 1,
+      "isRedshirt": false
+    }
+  ]
